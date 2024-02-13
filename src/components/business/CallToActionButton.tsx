b30f6bd0a1085f91d8ee105a945744d6d4f07335
Luis Leal <luisrrleal@gmail.com>
2024-02-13 10:05:31 -0600

    Redirect not working

--- src/components/business/CallToActionButton.tsx
+++ src/components/business/CallToActionButton.tsx
@@ -1,14 +1,17 @@
 'use client';
 
 import { createClick } from "@/actions";
+import { useRouter } from "next/navigation";
 
 export const CallToActionButton = ({ phone, fromBusinessId, toBusinessId }: { phone: string, fromBusinessId: number, toBusinessId: number }) => {
+
+    const router = useRouter();
 
     const handleClick = async () => {
         await createClick(fromBusinessId, toBusinessId);
 
         // Send to whatsapp and add the text 'Me gustaría tomar '
-        window.open(`https://api.whatsapp.com/send?phone=${phone}&text=¡Hola!,%20me%20gustaría%20la%20promoción%20que%20ofrece.`);
+        router.push(`https://api.whatsapp.com/send?phone=${phone}&text=¡Hola!,%20me%20gustaría%20la%20promoción%20que%20ofrece.`);
     }
 
     return (
